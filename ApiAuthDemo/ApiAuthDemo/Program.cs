d60646608bd521f4b19e31a4f908178d988319cc
Adam O'Neil <adamosoftware@gmail.com>
2024-05-17 07:09:42 -0400

    fixing an intermittent error

--- ApiAuthDemo/ApiAuthDemo/Program.cs
+++ ApiAuthDemo/ApiAuthDemo/Program.cs
@@ -77,7 +77,7 @@ app.UseAntiforgery();
 app.MapRazorComponents<App>()
 	.AddInteractiveServerRenderMode()
 	.AddInteractiveWebAssemblyRenderMode()
-	.AddAdditionalAssemblies(typeof(ApiAuthDemo.Client._Imports).Assembly);
+	.AddAdditionalAssemblies(typeof(ApiAuthDemo.Client.ApiClient).Assembly);
 
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
